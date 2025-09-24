5a3608c0b37096b402e505abb604840bc1147dbc
Vlad Iliescu <vlad@tallarium.com>
2025-09-24 02:14:50 +0100

    wrap non critical startup operations in try catch

--- src/Socket/socket.ts
+++ src/Socket/socket.ts
@@ -721,8 +721,12 @@ export const makeSocket = (config: SocketConfig) => {
 	})
 	// login complete
 	ws.on('CB:success', async() => {
-		await uploadPreKeysToServerIfRequired()
-		await sendPassiveIq('active')
+		try {
+			await uploadPreKeysToServerIfRequired()
+			await sendPassiveIq('active')
+		} catch (err) {
+			logger.warn({ err }, 'failed to send initial passive iq')
+		}
 
 		logger.info('opened connection to WA')
 		clearTimeout(qrTimer) // will never happen in all likelyhood -- but just in case WA sends success on first try
