fdb14f968e1d5718e383a02a68d62f4c5d33e94a
Vlad Iliescu <vlad@tallarium.com>
2026-02-06 20:00:55 +0000

    npm run build

--- lib/Socket/chats.d.ts
+++ lib/Socket/chats.d.ts
@@ -83,7 +83,7 @@ export declare const makeChatsSocket: (config: SocketConfig) => Promise<{
     logout: (msg?: string) => Promise<void>;
     end: (error: Error | undefined) => void;
     onUnexpectedError: (err: Error | Boom | import("axios").AxiosError, msg: string) => void;
-    uploadPreKeys: (count?: number) => Promise<void>;
+    uploadPreKeys: (count?: number, retryCount?: number) => Promise<void>;
     uploadPreKeysToServerIfRequired: () => Promise<void>;
     requestPairingCode: (phoneNumber: string, customPairingCode?: string) => Promise<string>;
     waitForConnectionUpdate: (check: (u: Partial<import("../Types").ConnectionState>) => Promise<boolean | undefined>, timeoutMs?: number) => Promise<void>;
