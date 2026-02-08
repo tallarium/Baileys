fd455c2f373c330ee882d64faef7ed3091e7ab6b
Aman Dhoot <aman@tallarium.com>
2026-02-08 21:04:08 +0000

    Support inline history bootstrap payload

--- src/Utils/history.ts
+++ src/Utils/history.ts
@@ -98,7 +98,13 @@ export const downloadAndProcessHistorySyncNotification = async (
 	msg: proto.Message.IHistorySyncNotification,
 	options: AxiosRequestConfig<{}>
 ) => {
-	const historyMsg = await downloadHistory(msg, options)
+	let historyMsg: proto.HistorySync
+	if (msg.initialHistBootstrapInlinePayload) {
+		historyMsg = proto.HistorySync.decode(await inflatePromise(msg.initialHistBootstrapInlinePayload))
+	} else {
+		historyMsg = await downloadHistory(msg, options)
+	}
+
 	return processHistoryMessage(historyMsg)
 }
 
